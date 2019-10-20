b1478d36f0f7c6f397db996fe4a6dd02cf5dd465
Han Kyung Yeop <yeop@dogu.xyz>
2019-10-20 22:13:35 +0900

    Upload 2019_agile_korea post
    (1/2)

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"블로그 첫글 테스트 입니닷.   이글의 제목은 Blog 첫글 테스트, 수정 시간은  입니다.  ","categories": ["Category"],
         "tags": ["tag1","tag2"],
         "url": "http://localhost:4000/category/new-post/",
+        "teaser":"http://localhost:4000/assets/images/logo_face.png"},{
+        "title": "2019 애자일 코리아 참가 후기 (1/2)",
+        "excerpt":"지난 10월 18일 코엑스에서 열린 2019 애자일 코리아 컨퍼런스에 다녀왔다. 다양한 분야에서 일하고 계신 분들의 애자일 조직으로 변화하는 과정에서 겪은 경험담을 들으며 글로만 배웠던 애자일이 SW 개발론을 넘어 기업문화 속에 어떻게 적용되고 있는지 느낄 수 있었다. Play Agile like an Adult: a story of mistakes from Fomula1 to bank Speaker...","categories": ["Soft Skill"],
+        "tags": ["Conference","agile"],
+        "url": "http://localhost:4000/soft%20skill/_2019_Korea_agile_conference_1_2/",
         "teaser":"http://localhost:4000/assets/images/logo_face.png"}]
