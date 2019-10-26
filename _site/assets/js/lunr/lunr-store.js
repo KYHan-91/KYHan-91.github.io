6d60e6ffead3daf4e80bf7b19a2b996bd52a61a1
Han Kyung Yeop <yeop@dogu.xyz>
2019-10-26 19:58:19 +0900

    Edit Bugs

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,30 +2,15 @@ var store = [{
         "title": "Blog 첫글 테스트",
         "excerpt":"블로그 첫글 테스트 입니닷.   이글의 제목은 Blog 첫글 테스트, 수정 시간은  입니다.  ","categories": ["Category"],
         "tags": ["tag1","tag2"],
-        "url": "http://localhost:4000/blog/category/new-post/",
-        "teaser":"http://localhost:4000/blog/assets/images/logo_face.png"},{
+        "url": "http://localhost:4000/category/new-post/",
+        "teaser":"http://localhost:4000/assets/images/logo_face.png"},{
         "title": "2019 애자일 코리아 참가 후기 (1/n)",
         "excerpt":"지난 10월 18일 코엑스에서 열린 2019 애자일 코리아 컨퍼런스에 다녀왔다. 다양한 분야에서 일하고 계신 분들의 애자일 조직으로 변화하는 과정에서 겪은 경험담을 들으며 글로만 배웠던 애자일이 SW 개발론을 넘어 기업문화 속에 어떻게 적용되고 있는지 느낄 수 있었다. Toggle menu 2019 Agile Conference 관련 글 목록 Play Agile like an Adult: a...","categories": ["Soft Skill"],
         "tags": ["Conference","agile"],
-        "url": "http://localhost:4000/blog/soft%20skill/2019_Korea_agile_conference_1_n/",
-        "teaser":"http://localhost:4000/blog/assets/images/logo_face.png"},{
+        "url": "http://localhost:4000/soft%20skill/2019_Korea_agile_conference_1_n/",
+        "teaser":"http://localhost:4000/assets/images/logo_face.png"},{
         "title": "2019 애자일 코리아 참가 후기 (2/n)",
         "excerpt":"지난 10월 18일 코엑스에서 열린 2019 애자일 코리아 컨퍼런스에 다녀왔다. 다양한 분야에서 일하고 계신 분들의 애자일 조직으로 변화하는 과정에서 겪은 경험담을 들으며 글로만 배웠던 애자일이 SW 개발론을 넘어 기업문화 속에 어떻게 적용되고 있는지 느낄 수 있었다. 애자일 개발 조직으로 변화, 도전과 성과 (삼성의료기기_김효&amp;박성남) 특이했던 점은 시작부터 술자리에서 선배가 해주는 말처럼...","categories": ["Soft Skill"],
         "tags": ["Conference","agile"],
-        "url": "http://localhost:4000/blog/soft%20skill/2019_Korea_agile_conference_2_n/",
-        "teaser":"http://localhost:4000/blog/assets/images/logo_face.png"},{
-        "title": "2019 애자일 코리아 참가 후기 (1/n)",
-        "excerpt":"지난 10월 18일 코엑스에서 열린 2019 애자일 코리아 컨퍼런스에 다녀왔다. 다양한 분야에서 일하고 계신 분들의 애자일 조직으로 변화하는 과정에서 겪은 경험담을 들으며 글로만 배웠던 애자일이 SW 개발론을 넘어 기업문화 속에 어떻게 적용되고 있는지 느낄 수 있었다. Toggle menu 2019 Agile Conference 관련 글 목록 Play Agile like an Adult: a...","categories": ["Soft Skill"],
-        "tags": ["Conference","agile"],
-        "url": "http://localhost:4000/blog/softskill/2019_Korea_agile_conference/2019-10-20-2019_Korea_agile_conference_1_n/",
-        "teaser":"http://localhost:4000/blog/assets/images/logo_face.png"},{
-        "title": "2019 애자일 코리아 참가 후기 (2/n)",
-        "excerpt":"지난 10월 18일 코엑스에서 열린 2019 애자일 코리아 컨퍼런스에 다녀왔다. 다양한 분야에서 일하고 계신 분들의 애자일 조직으로 변화하는 과정에서 겪은 경험담을 들으며 글로만 배웠던 애자일이 SW 개발론을 넘어 기업문화 속에 어떻게 적용되고 있는지 느낄 수 있었다. 애자일 개발 조직으로 변화, 도전과 성과 (삼성의료기기_김효&amp;박성남) 특이했던 점은 시작부터 술자리에서 선배가 해주는 말처럼...","categories": ["Soft Skill"],
-        "tags": ["Conference","agile"],
-        "url": "http://localhost:4000/blog/softskill/2019_Korea_agile_conference/2019-10-26-2019_Korea_agile_conference_2_n/",
-        "teaser":"http://localhost:4000/blog/assets/images/logo_face.png"},{
-        "title": "Foo Bar Identity",
-        "excerpt":" ","categories": ["Soft Skill"],
-        "tags": ["Conference","agile"],
-        "url": "http://localhost:4000/blog/softskill/2019_Korea_agile_conference/sample/",
-        "teaser":"http://localhost:4000/blog/assets/images/logo_face.png"}]
+        "url": "http://localhost:4000/soft%20skill/2019_Korea_agile_conference_2_n/",
+        "teaser":"http://localhost:4000/assets/images/logo_face.png"},]
